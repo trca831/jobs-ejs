e70fd9b89f9758fa0284e9dd2a893e1e20226718
Tracy Cano <tracycano3.14@gmail.com>
2025-06-03 23:04:32 -0700

    flash messages

--- app.js
+++ app.js
@@ -38,14 +38,25 @@ if (app.get("env") === "production") {
 
 app.use(session(sessionParms));
 
+// flash messages (depends on session being set up)
+app.use(require("connect-flash")());
+
 app.get("/secretWord", (req, res) => {
     if (!req.session.secretWord) {
       req.session.secretWord = "syzygy";
     }
+    res.locals.info = req.flash("info");
+    res.locals.errors = req.flash("error");
     res.render("secretWord", { secretWord: req.session.secretWord });
   });
   app.post("/secretWord", (req, res) => {
-    req.session.secretWord = req.body.secretWord;
+    if (req.body.secretWord.toUpperCase()[0] == "P") {
+      req.flash("error", "That word won't work!");
+      req.flash("error", "You can't use words that start with p.");
+    } else {
+      req.session.secretWord = req.body.secretWord;
+      req.flash("info", "The secret word was changed.");
+    }
     res.redirect("/secretWord");
   });
 

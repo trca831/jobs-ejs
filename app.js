67ad51b617ee06f3f36f71f7f190dea3e9654c4c
Tracy Cano <tracycano3.14@gmail.com>
2025-06-03 22:58:05 -0700

    sessions

--- app.js
+++ app.js
@@ -7,14 +7,47 @@ app.set("view engine", "ejs");
 app.use(require("body-parser").urlencoded({ extended: true }));
 
 // secret word handling
-let secretWord = "syzygy";
+// let secretWord = "syzygy";
+
+require("dotenv").config(); // to load the .env file into the process.env object
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const url = process.env.MONGO_URI;
+
+const store = new MongoDBStore({
+  // may throw an error, which won't be caught
+  uri: url,
+  collection: "mySessions",
+});
+store.on("error", function (error) {
+  console.log(error);
+});
+
+const sessionParms = {
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: store,
+  cookie: { secure: false, sameSite: "strict" },
+};
+
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sessionParms.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionParms));
+
 app.get("/secretWord", (req, res) => {
-  res.render("secretWord", { secretWord });
-});
-app.post("/secretWord", (req, res) => {
-  secretWord = req.body.secretWord;
-  res.redirect("/secretWord");
-});
+    if (!req.session.secretWord) {
+      req.session.secretWord = "syzygy";
+    }
+    res.render("secretWord", { secretWord: req.session.secretWord });
+  });
+  app.post("/secretWord", (req, res) => {
+    req.session.secretWord = req.body.secretWord;
+    res.redirect("/secretWord");
+  });
 
 app.use((req, res) => {
   res.status(404).send(`That page (${req.url}) was not found.`);
